295bdc52d96e928739e07239dcbad02b4ee4201d
marco caneva <marco.caneva.dev@outlook.com>
2023-04-26 23:13:06 +0200

    finito es6

--- es5.py
+++ es5.py
@@ -4,14 +4,17 @@ s = Server().boot()
 s.setAmp(0.2)
 
 env = Adsr(dur=1.5)
-env.ctrl()
+env.play()
 
-wf = LFO(freq=200,sharp=1,mul= env).mix(2).out()
+wf = LFO(freq=200,sharp=1,mul= env)
 
 def loo():
     env.play()
+    
 
-p = Pattern(function = loo,time = 1)
+p = Pattern(function = loo,time = 1).play()
+
+wf = Pan(wf).out()
 sc = Scope([wf,env])
 
 s.gui(locals())
--- es6.py
+++ es6.py
@@ -8,17 +8,33 @@ ns.keyboard()
 env=MidiAdsr(ns["velocity"])
 env.ctrl()
 
-l1=LFO(mul=.1,add=1)
-l2=LFO(mul=.4,add=1)
-l1.ctrl()
-l2.ctrl()
+mm1=SLMap(4,8,'lin','freq',4)
+mf1=SLMap(0,0.5,'lin','mul',0.01)
+
+mm2=SLMap(1,10,'lin','freq',4)
+mf2=SLMap(0,0.8,'lin','mul',0.01)
+
+l1=LFO(type=3,mul=.1,add=1)
+l2=LFO(type=3,mul=.4,add=1)
+l1.ctrl(map_list=[mm1,mf1],title="vibrato")
+l2.ctrl(map_list=[mm2,mf2],title='tremolo')
 
 
 wf = LFO(freq = l1*ns["pitch"],mul = env*l2)
-wf.ctrl()
+wf.ctrl(title='waveform')
 
+wfm1 =SLMap(20,20000,'log','freq',12000)
+wfm2 =SLMap(0,5,'lin','res',0.5)
 wff = MoogLP(wf)
+wff.ctrl(map_list=[wfm1,wfm2],title='LP Filter')
 
-o = Pan(wf).out()
+smap=SLMap(0,1,'lin','value',0.5)
+set=Sig(0.5)
+set.ctrl(map_list=[smap],title='Dry/Wet')
+
+o = Selector(inputs=[wf,wff],voice=set)
+o = Pan(o).out()
+
+sc=Scope(o)
 
 s.gui(locals())
