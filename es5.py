b83b83fed83d20d09308f3f2cc5278ea16028c4e
marco caneva <marco.caneva.dev@outlook.com>
2023-04-26 23:43:38 +0200

    final touches

--- es5.py
+++ es5.py
@@ -3,16 +3,23 @@ from pyo import *
 s = Server().boot()
 s.setAmp(0.2)
 
-env = Adsr(dur=1.5)
-env.play()
 
-wf = LFO(freq=200,sharp=1,mul= env)
+
+t = Sig(1)
+t.ctrl(title='tempo')
+
+env = Adsr(0.01,0.1,0.3,2.6,1.5)
+env.ctrl()
+
+freqs= [261.63, 293.66,329.63,349.23,392.0,440.0,493.88]
+
+wf = LFO(freq=Choice(freqs,freq=1/t),sharp=1,mul= env)
 
 def loo():
     env.play()
     
 
-p = Pattern(function = loo,time = 1).play()
+p = Pattern(function = loo,time = t ).play()
 
 wf = Pan(wf).out()
 sc = Scope([wf,env])
